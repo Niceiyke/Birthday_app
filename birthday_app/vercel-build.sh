44e821d516efcceb81bf6bd1d00b5cd62f47af1d
diyke04 <iykedave04@gmail.com>
2024-11-11 15:22:04 +0100

    updated

--- birthday_app/vercel-build.sh
+++ birthday_app/vercel-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 # Run migrations
 python manage.py migrate --noinput
 
-# Collect static files (if necessary)
-python manage.py collectstatic --noinput
+# Create the superuser (you can set environment variables to avoid manual input)
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
