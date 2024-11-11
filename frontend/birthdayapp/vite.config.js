343380148ad38b926372ff9e0e1eecddd61e66c7
diyke04 <iykedave04@gmail.com>
2024-11-11 13:06:44 +0100

    updated

--- frontend/birthdayapp/vite.config.js
+++ frontend/birthdayapp/vite.config.js
@@ -1,10 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/birthdayapp/', // Replace with your subdirectory path
   plugins: [react()],
-});
+})
+
